c41ba8c4b7fe35c8a3fa62410008e9e0fe40bef1
spyder005 <manasc532@gmail.com>
2024-10-13 09:46:02 +0530

    datacommit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port =  3000;
+
+// Serve JSON-LD data
+app.get('/blog', (req, res) => {
+  res.sendFile(path.join(__dirname, 'data', 'blog.jsonld'));
+});
+
+app.get('/jobposting', (req, res) => {
+  res.sendFile(path.join(__dirname, 'data', 'jobposting.jsonld'));
+});
+
+app.get('/testimonial', (req, res) => {
+  res.sendFile(path.join(__dirname, 'data', 'testimonial.jsonld'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'data', 'about.jsonld'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
